e8bbd7081747ebef6390ce97faa9299ba65105b0
Evan Mercurio <EMercurio@tql.com>
2022-05-03 11:11:22 -0400

    Final commit

--- CalculusHomework/Program.cs
+++ CalculusHomework/Program.cs
@@ -17,4 +17,21 @@ Console.WriteLine(DivResult);
 Console.WriteLine($"{num01} + {num02} = {SumResult}");
 Console.WriteLine($"{num05} - {num03} = {SubResult}");
 Console.WriteLine($"{num05} * {num04} = {MultResult}");
-Console.WriteLine($"{num04} / {num02} = {DivResult}");
+Console.WriteLine($"{num04} / {num02} = {DivResult}"); 
+
+{
+    var aVariable = 0;
+    {
+        var bVariable = aVariable;
+    }
+    var cVariable = aVariable;
+}
+
+// the number of slots in an Array is set. cannot grow. ONLY allows same type. String, bool, int, 
+int[] nbrs = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110 }; // there is 12 slots of integer values. first slot is = 0 slot. zero based. last slot is 11
+int[] nbrs2 = new int[10]; //10 slots are allocated. there is no data inputted yet. 10 slots filled with zero's.
+
+var thirty = nbrs[3];
+
+nbrs2[5] = 6; //this is setting the 6th slot the number 6
+
